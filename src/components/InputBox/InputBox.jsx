5589333d953926baec78a0563daad06407fe7278
akashmondal2000 <mondalakash494@gmail.com>
2024-05-12 20:33:25 +0530

    create input box

--- src/components/InputBox/InputBox.jsx
+++ src/components/InputBox/InputBox.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import styles from "./inputBox.module.css";
 
+const Currency = ['inr','usd','kdg']
+
 const InputBox = () => {
   return (
-    <div className={styles.InputBox_Wrapper}>
-      <input type="text" placeholder="name" name="name" />
+    <div className={styles.inputBox_container}>
+      <div className={styles.input_wrapper}>
+        <label htmlFor="" className={styles.input_label}>
+          usd
+        </label>
+        <input type="number" className={styles.inputBox} />
+      </div>
+      <div className={styles.inputBox_wrapper_select}>
+      <p className={styles.inputBox_wrapper_select_text}>Currency type</p>
+      <select className={styles.inputBox_select}
+      
+      >
+      {Currency.map((currency)=>(
+        <option key={currency} value={currency}>
+          {currency}
+        </option>
+      ))}
+
+      </select>
+
+      </div>
     </div>
   );
 };
